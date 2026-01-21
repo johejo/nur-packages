dc1b888fa2a58f93333c169ee35e5b36011fc79f
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-01-21 19:47:34 +0900

    fix build

--- pkgs/test/default.nix
+++ pkgs/test/default.nix
@@ -1,4 +1,9 @@
-{ writeShellScriptBin }:
-writeShellScriptBin "test" ''
-  echo "test"
-''
+{ stdenv }:
+
+stdenv.mkDerivation rec {
+  name = "test-${version}";
+  version = "1.0";
+  src = ./.;
+  buildPhase = "echo echo Hello World > example";
+  installPhase = "install -Dm755 example $out/bin/test";
+}
