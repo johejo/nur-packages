c2310100c38ea751f49ca81b91b7d6150b1f3ff2
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-01-27 23:40:17 +0900

    add meta

--- pkgs/gitbucket/default.nix
+++ pkgs/gitbucket/default.nix
@@ -1,4 +1,8 @@
-{ stdenvNoCC, source }:
+{
+  lib,
+  stdenvNoCC,
+  source,
+}:
 
 stdenvNoCC.mkDerivation rec {
   inherit (source) pname version src;
@@ -7,4 +11,10 @@ stdenvNoCC.mkDerivation rec {
     mkdir -p $out/lib
     cp ${src} $out/lib/gitbucket.war
   '';
+  meta = {
+    description = "A Git platform powered by Scala with easy installation, high extensibility & GitHub API compatibility";
+    homepage = "https://github.com/gitbucket/gitbucket";
+    license = lib.licenses.asl20;
+    changelog = "https://github.com/gitbucket/gitbucket/releases/tag/${version}";
+  };
 }
