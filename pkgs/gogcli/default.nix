ba7e5d7e68b5911656dd3127c96fd80da1109d3e
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-01-28 16:03:52 +0900

    add checkFlags for gogscli to skip network-dependent tests

--- pkgs/gogcli/default.nix
+++ pkgs/gogcli/default.nix
@@ -8,7 +8,38 @@
 buildGoModule rec {
   inherit (source) pname version src;
   vendorHash = "sha256-nig3GI7eM1XRtIoAh1qH+9PxPPGynl01dCZ2ppyhmzU=";
-  doCheck = false; # Tests require network access
+  checkFlags =
+    let
+      skippedTests = [
+        # internal/googleauth - OAuth flow tests requiring network/ports
+        "TestAuthorize_Manual_Success"
+        "TestAuthorize_Manual_Success_NoNewline"
+        "TestAuthorize_Manual_CancelEOF"
+        "TestAuthorize_Manual_StateMismatch"
+        "TestAuthorize_ServerFlow_Success"
+        "TestAuthorize_ServerFlow_CallbackErrors"
+        "TestManageServer_HandleOAuthCallback_ErrorAndValidation"
+        "TestManageServer_HandleOAuthCallback_Success"
+        "TestManageServer_HandleOAuthCallback_FileBackendSkipsKeychain"
+        "TestManageServer_HandleOAuthCallback_Success_IDTokenEmail"
+        "TestManageServer_HandleAuthStart"
+        "TestManageServer_HandleAuthUpgrade"
+        "TestFetchUserEmail"
+        "TestStartManageServer_Timeout"
+        "TestManageServerHandleOAuthCallback_ReadCredsError"
+        "TestManageServerHandleOAuthCallback_ScopesError"
+        "TestManageServerHandleOAuthCallback_ExchangeError"
+        "TestManageServerHandleOAuthCallback_MissingRefreshToken"
+        "TestManageServerHandleOAuthCallback_FetchEmailError"
+        "TestCheckRefreshTokenSuccess"
+        "TestCheckRefreshTokenFailure"
+        # internal/cmd - Gmail watch tests requiring network
+        "TestGmailWatchStartCmd_JSON"
+        "TestGmailWatchServeCmd_UsesStoredHook"
+        "TestGmailWatchServeCmd_DefaultMaxBytes"
+      ];
+    in
+    [ "-skip=^${lib.concatStringsSep "$|^" skippedTests}$" ];
   ldflags =
     let
       mod = "github.com/steipete/gogcli/internal/cmd";
