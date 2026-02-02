335986820855b804eab374cf7201f8fbb3c2122f
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-02-02 22:54:27 +0900

    add caddy-l4 plugin

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -5,6 +5,7 @@ caddy.withPlugins {
     "github.com/caddy-dns/cloudflare@v0.2.2"
     "github.com/caddyserver/replace-response@v0.0.0-20250618171559-80962887e4c6"
     "github.com/mholt/caddy-dynamicdns@v0.0.0-20251231002810-1af4f8876598"
+    "github.com/mholt/caddy-l4@v0.0.0-20260127203130-040d25cc886a"
   ];
-  hash = "sha256-ETBSipfa6qtuIiGy+mhS8jFlklOv1GBXNKKyXYG9YT8=";
+  hash = "sha256-jBO18FFNd14J4afY1Rl50JOgkaauHEwPDHRyCI2bL0Y=";
 }
