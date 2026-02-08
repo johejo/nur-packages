172336558eccf7fbc7da738731b5c73066383bff
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-02-08 16:45:42 +0900

    fix berkeleydb linux build

--- pkgs/apple-oss-distributions/berkeleydb/default.nix
+++ pkgs/apple-oss-distributions/berkeleydb/default.nix
@@ -9,10 +9,17 @@ stdenv.mkDerivation {
   inherit (source) pname version src;
   sourceRoot = "source/db";
 
+  NIX_CFLAGS_COMPILE = lib.optionals stdenv.isLinux [ "-Wno-old-style-definition" ];
+
   preConfigure = ''
     cd build_unix
     configureScript=../dist/configure
   '';
+
+  configureFlags = lib.optionals stdenv.isLinux [
+    "--enable-posixmutexes"
+    "db_cv_mutex=POSIX/pthreads"
+  ];
 
   enableParallelBuilding = true;
 
