d89d29fdb45f28a682414a91472c06702a367e12
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-02-26 20:35:02 +0900

    Simplify CI nix build import

--- ci.nix
+++ ci.nix
@@ -10,7 +10,12 @@
 # which this is possible.
 
 {
-  pkgs ? import <nixpkgs> { },
+  pkgs ?
+    let
+      flake = builtins.getFlake (toString ./.);
+      system = builtins.currentSystem;
+    in
+    import flake.inputs.nixpkgs { inherit system; },
 }:
 
 with builtins;
