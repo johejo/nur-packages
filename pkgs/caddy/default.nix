47cfd13c2f86fd6b5c45bbbb62c403f6884efffc
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-02-26 21:49:00 +0900

    caddy: update vendor hash

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -7,5 +7,5 @@ caddy.withPlugins {
     "github.com/mholt/caddy-dynamicdns@v0.0.0-20251231002810-1af4f8876598"
     "github.com/mholt/caddy-l4@v0.0.0-20260127203130-040d25cc886a"
   ];
-  hash = "sha256-jBO18FFNd14J4afY1Rl50JOgkaauHEwPDHRyCI2bL0Y=";
+  hash = "sha256-iJUiJ9pa8DCh93YCwIeVyJo98Z8Qi58FhI2SBR70J3Y=";
 }
