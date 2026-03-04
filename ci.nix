f5c7f7dbfca97df088e62e50d120b06e5c3cc9b8
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-03-04 16:47:51 +0900

    Add platform check to CI filters

--- ci.nix
+++ ci.nix
@@ -28,7 +28,9 @@ let
       licenseFromMeta = p.meta.license or [ ];
       licenseList = if builtins.isList licenseFromMeta then licenseFromMeta else [ licenseFromMeta ];
     in
-    !(p.meta.broken or false) && builtins.all (license: license.free or true) licenseList;
+    !(p.meta.broken or false)
+    && builtins.all (license: license.free or true) licenseList
+    && pkgs.lib.meta.availableOn pkgs.stdenv.hostPlatform p;
   isCacheable = p: !(p.preferLocalBuild or false);
   shouldRecurseForDerivations = p: isAttrs p && p.recurseForDerivations or false;
 
