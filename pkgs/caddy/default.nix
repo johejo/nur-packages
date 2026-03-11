1a0b51ad8eca7a0fc1c0212d43a54b7aefdb8ba4
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-03-11 18:39:25 +0900

    update caddy and nixpkgs

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -5,7 +5,6 @@ caddy.withPlugins {
     "github.com/caddy-dns/cloudflare@v0.2.2"
     "github.com/caddyserver/replace-response@v0.0.0-20250618171559-80962887e4c6"
     "github.com/mholt/caddy-dynamicdns@v0.0.0-20251231002810-1af4f8876598"
-    "github.com/mholt/caddy-l4@v0.0.0-20260127203130-040d25cc886a"
   ];
-  hash = "sha256-iJUiJ9pa8DCh93YCwIeVyJo98Z8Qi58FhI2SBR70J3Y=";
+  hash = "sha256-Cw3/MQ0woeJVDY9RnPHmbSznwa7fHZ/qqQIQ5yeBpUk=";
 }
