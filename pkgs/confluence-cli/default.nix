808323a74598d3ca5136d1f7ebbc04ba0e1ac4d8
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-04-21 14:36:34 +0900

    Run confluence-cli with bun

--- pkgs/confluence-cli/default.nix
+++ pkgs/confluence-cli/default.nix
@@ -1,5 +1,7 @@
 {
+  bun,
   buildNpmPackage,
+  makeWrapper,
   source,
   ...
 }:
@@ -9,11 +11,23 @@ buildNpmPackage {
 
   npmDepsHash = "sha256-3D9OBvtJ2Kv7IhTX/vv4+txxBCDL/x4gh+094Tv55FU=";
 
+  nativeBuildInputs = [
+    makeWrapper
+  ];
+
   postPatch = ''
     cp npm-shrinkwrap.json package-lock.json
   '';
 
   dontNpmBuild = true;
+
+  postFixup = ''
+    for bin in confluence confluence-cli; do
+      rm "$out/bin/$bin"
+      makeWrapper ${bun}/bin/bun "$out/bin/$bin" \
+        --add-flags "$out/lib/node_modules/confluence-cli/bin/index.js"
+    done
+  '';
 
   meta = {
     mainProgram = "confluence";
