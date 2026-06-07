37d99626cfe2f8cc64d51d430e4f3e5e0b043863
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-06-07 19:41:33 +0900

    update caddy

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -2,9 +2,9 @@
 
 caddy.withPlugins {
   plugins = [
-    "github.com/caddy-dns/cloudflare@v0.2.2"
+    "github.com/caddy-dns/cloudflare@v0.2.4"
     "github.com/caddyserver/replace-response@v0.0.0-20250618171559-80962887e4c6"
     "github.com/mholt/caddy-dynamicdns@v0.0.0-20251231002810-1af4f8876598"
   ];
-  hash = "sha256-RaJve8YxFKA62++M+6m1WAa5t+vI+XCmTuEetGc9/Zw=";
+  hash = "sha256-x0cBCEc2bRBUbAX8d/Pxku6ieQscnQVO8Lh4TkNbKps=";
 }
