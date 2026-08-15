37bc987fa7234d3ed4ad5b0d5d730415ea9ed68a
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-08-15 09:53:37 +0900

    bump caddy

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -6,5 +6,5 @@ caddy.withPlugins {
     "github.com/caddyserver/replace-response@v0.0.0-20250618171559-80962887e4c6"
     "github.com/mholt/caddy-dynamicdns@v0.0.0-20251231002810-1af4f8876598"
   ];
-  hash = "sha256-x0cBCEc2bRBUbAX8d/Pxku6ieQscnQVO8Lh4TkNbKps=";
+  hash = "sha256-p2vcm0TRj/G2SOjTC6TERi8bnEg9iziaunMzXRRkVUk=";
 }
