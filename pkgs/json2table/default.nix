cbdd9bc77865d0965a159bacdeabfcb4b3291c47
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-08-22 16:01:51 +0900

    json2table: run with minimal Deno permissions

--- pkgs/json2table/default.nix
+++ pkgs/json2table/default.nix
@@ -1,8 +1,9 @@
 {
   lib,
   stdenvNoCC,
-  nodejs,
+  deno,
   fetchFromGitHub,
+  makeWrapper,
   nix-update-script,
   ...
 }:
@@ -17,12 +18,28 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-WQLtjLsLr1U1rL3xEfkyeKiCUadibD2Hi112bIGmzQs=";
   };
 
-  nativeBuildInputs = [ nodejs ];
+  nativeBuildInputs = [ makeWrapper ];
 
   installPhase = ''
     runHook preInstall
-    install -Dm755 json2table.js $out/bin/json2table
+    install -Dm444 json2table.js $out/libexec/json2table/json2table.js
+    makeWrapper ${lib.getExe deno} $out/bin/json2table \
+      --set DENO_NO_UPDATE_CHECK 1 \
+      --add-flags run \
+      --add-flags --no-config \
+      --add-flags --cached-only \
+      --add-flags $out/libexec/json2table/json2table.js
     runHook postInstall
+  '';
+
+  doInstallCheck = true;
+  installCheckPhase = ''
+    runHook preInstallCheck
+    $out/bin/json2table --help | grep -F "Usage: json2table"
+    printf '%s\n' '[{"name":"John","age":30}]' | $out/bin/json2table > table
+    grep -F "John" table
+    grep -F "30" table
+    runHook postInstallCheck
   '';
 
   passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch=main" ]; };
