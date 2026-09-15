bcbe8bb8868162931cf0ee719807e506d284899e
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-09-15 17:40:39 +0900

    support for caddy plugins with nix-update

--- pkgs/caddy/default.nix
+++ pkgs/caddy/default.nix
@@ -1,10 +1,21 @@
-{ caddy, ... }:
+{ caddy, nix-update-script, ... }:
 
-caddy.withPlugins {
+(caddy.withPlugins {
   plugins = [
     "github.com/caddy-dns/cloudflare@v0.2.4"
     "github.com/caddyserver/replace-response@v0.0.0-20250618171559-80962887e4c6"
     "github.com/mholt/caddy-dynamicdns@v0.0.0-20251231002810-1af4f8876598"
   ];
-  hash = "sha256-p2vcm0TRj/G2SOjTC6TERi8bnEg9iziaunMzXRRkVUk=";
-}
+  hash = "sha256-6I8AOZ84DO4iyCsefBSQH5jZkdPwuZkIfnkJdVCT6O4=";
+}).overrideAttrs
+  (old: {
+    # Keep source positions local so nix-update can update the plugin source hash.
+    version = caddy.version;
+    src = old.src;
+
+    passthru = old.passthru // {
+      updateScript = nix-update-script {
+        extraArgs = [ "--version=skip" ];
+      };
+    };
+  })
