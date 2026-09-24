fa659c6cb4cde1052a551b9de90a9d2ae9ac56e6
Mitsuo HEIJO <mitsuo.heijo@gmail.com>
2026-09-24 22:01:36 +0900

    drop terminal-browser-bin

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -75,7 +75,6 @@ in
   acli-bin = pkgs.callPackage ./acli-bin { };
   shelley-bin = pkgs.callPackage ./shelley-bin { };
   socktainer-bin = pkgs.callPackage ./socktainer-bin { };
-  terminal-browser-bin = pkgs.callPackage ./terminal-browser-bin { };
   pkgsite = pkgs.callPackage ./pkgsite { };
 }
 //
